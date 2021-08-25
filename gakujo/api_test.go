9d71bf94213111fe6649735a7edfc5ce33042cb3
kaji <kajiharalivi@icloud.com>
2021-08-25 17:44:52 +0900

    hoge

--- gakujo/api_test.go
+++ gakujo/api_test.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
-	"github.com/Cside/jsondiff"
 	"github.com/joho/godotenv"
 )
 
@@ -16,6 +16,7 @@ var (
 	begin    time.Time
 	username string
 	password string
+	c        *Client
 )
 
 func init() {
@@ -26,21 +27,34 @@ func init() {
 	username = os.Getenv("J_USERNAME")
 	password = os.Getenv("J_PASSWORD")
 	begin = time.Now()
+	c = NewClient()
+	if err := c.Login(username, password); err != nil {
+		log.Fatal("failed to login")
+	}
+	log.Println("[Info]Login succeeded(took:", time.Since(begin), "ms)")
+}
+
+// JSONBytesEqual compares the JSON in two byte slices.
+func JSONBytesEqual(a, b []byte) (bool, error) {
+	var j, j2 interface{}
+	if err := json.Unmarshal(a, &j); err != nil {
+		return false, err
+	}
+	if err := json.Unmarshal(b, &j2); err != nil {
+		return false, err
+	}
+	return reflect.DeepEqual(j2, j), nil
 }
 
 func TestLogin(t *testing.T) {
-	c := NewClient()
-	if err := c.Login(username, password); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestHome(t *testing.T) {
-	c := NewClient()
-	if err := c.Login(username, password); err != nil {
+	inc := NewClient()
+	if err := inc.Login(username, password); err != nil {
 		t.Fatal(err)
 	}
 	t.Log("[Info]Login succeeded(took:", time.Since(begin), "ms)")
+}
+
+func TestHome(t *testing.T) {
 	homeInfo, err := c.Home()
 	if err != nil {
 		t.Fatal(err)
@@ -124,14 +138,48 @@ func TestClassNoticeRow(t *testing.T) {
 	}
 	fmt.Println(string(e))
 	classnoticerow := classNoticeRow
-	classnoticerow[10].CourseName = "hogehoge"
+	// classnoticerow[10].CourseName = "hogehoge"
 	e2, err := json.MarshalIndent(classnoticerow, "", " ")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if diff := jsondiff.Diff(e, e2); diff == "" {
+	if diff, _ := JSONBytesEqual(e, e2); diff == true {
 		fmt.Println("OK")
 	} else {
 		fmt.Println("NG")
 	}
+	f, err := os.Create("../hiniti.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.WriteString(string(e))
+	f2, err := os.Create("../hiniti2.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	f2.WriteString(string(e2))
+}
+
+func TestSeisekiRows(t *testing.T) {
+	kc, err := c.NewKyoumuClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows, err := kc.SeisekiRows()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, row := range rows {
+		fmt.Println(*row)
+	}
+	e, err := json.MarshalIndent(rows, "", " ")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(string(e))
+	f, err := os.Create("seiseki.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.WriteString(string(e))
 }
