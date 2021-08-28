2bff9a101c9e898565efbf1259dc668cb76413e8
kaji <kajiharalivi@icloud.com>
2021-08-29 00:02:46 +0900

    hoge

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 
-	"github.com/joho/godotenv"
 	"github.com/kajikaji0725/gakujo_Slack/slack_bot"
 	"github.com/szpp-dev-team/gakujo-api/gakujo"
 )
@@ -13,11 +11,11 @@ import (
 var Api []string
 
 func main() {
-	flag.Parse()
-	fmt.Println(flag.Args())
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal("please set .env on ./..", err)
-	}
+	// flag.Parse()
+	// fmt.Println(flag.Args())
+	// if err := godotenv.Load(".env"); err != nil {
+	// 	log.Fatal("please set .env on ./..", err)
+	// }
 	Api = flag.Args()
 	c := gakujo.NewClient()
 	if err := c.Login(Api[0], Api[1]); err != nil {
