4be8a240393568db9fbce52abc929058e6df2bac
kaji <kajiharalivi@icloud.com>
2021-08-28 10:55:57 +0900

    randomにも送る

--- slack_bot/bot.go
+++ slack_bot/bot.go
@@ -9,7 +9,6 @@ import (
 )
 
 func BotNew(seiseki []*model.SeisekiRow, change []*model.SeisekiRow) {
-	fmt.Println(os.Getenv("BOT"))
 	messeages := ""
 	changeMessages := ""
 	for _, row := range seiseki {
@@ -23,7 +22,7 @@ func BotNew(seiseki []*model.SeisekiRow, change []*model.SeisekiRow) {
 		changeMessages += changemesseage
 	}
 	changeMessages += "これが追加されました"
-	api := slack.New(os.Getenv("BOT"))
+	api := slack.New(os.Getenv("BOT_TOKEN"))
 	_, _, _ = api.PostMessage(
 		os.Getenv("BOT_CHANNEL"),
 		slack.MsgOptionText(messeages, false),
@@ -32,11 +31,21 @@ func BotNew(seiseki []*model.SeisekiRow, change []*model.SeisekiRow) {
 		os.Getenv("BOT_CHANNEL"),
 		slack.MsgOptionText(changeMessages, false),
 	)
+	api = slack.New("xoxb-1491378823348-2286618846368-uIxUrd1gGmuRC2QBPe1euDjr")
+	messeages = ""
+	for _, row := range change {
+		messeage := fmt.Sprintf("%v\n", row.SubjectName)
+		messeages += messeage
+	}
+	messeages += "この科目の成績が追加されました。"
+	_, _, _ = api.PostMessage(
+		"random",
+		slack.MsgOptionText(messeages, false),
+	)
 }
 
 func BotSame() {
-	fmt.Println(os.Getenv("BOT"))
-	api := slack.New(os.Getenv("BOT"))
+	api := slack.New(os.Getenv("BOT_TOKEN"))
 	_, _, _ = api.PostMessage(
 		os.Getenv("BOT_CHANNEL"),
 		slack.MsgOptionText("成績に変更はありません", false),
