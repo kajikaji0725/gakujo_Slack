09472dc89c91363045afe4c8806517c0db1d7fa0
kaji <kajiharalivi@icloud.com>
2021-09-04 03:30:26 +0900

    hoge

--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/kajikaji0725/gakujo_Slack/slack_bot"
 	"github.com/szpp-dev-team/gakujo-api/gakujo"
 )
 
 func main() {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal("please set .env on ./..", err)
-	}
+	// if err := godotenv.Load(".env"); err != nil {
+	// 	log.Fatal("please set .env on ./..", err)
+	// }
 	c := gakujo.NewClient()
 	if err := c.Login(os.Getenv("J_USERNAME"), os.Getenv("J_PASSWORD")); err != nil {
 		log.Fatal(err)
