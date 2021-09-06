4ac6f2a1cb8cb58128b0705b6d15e27c0d82e7d1
kaji <kajiharalivi@icloud.com>
2021-09-06 10:52:03 +0900

    hoge

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func init() {
 
 func main() {
 	cr := cron.New()
-	cr.AddFunc("*/5 * * * *", func() {
+	cr.AddFunc("00 */10 * * *", func() {
 		c := gakujo.NewClient()
 		if err := c.Login(os.Getenv("J_USERNAME"), os.Getenv("J_PASSWORD")); err != nil {
 			log.Fatal(err)
